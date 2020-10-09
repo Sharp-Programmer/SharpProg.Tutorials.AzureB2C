175141d3a06f0b0403cfc6412e02df655de7c491
azizkap21 <aziz@azizweb.com>
2020-10-09 10:07:23 +0100

    Some app settings comments and startup file changes

--- src/SharpProg.Tutorials.AzureB2C.WebClient/ProductApiSettings.cs
+++ src/SharpProg.Tutorials.AzureB2C.WebClient/ProductApiSettings.cs
@@ -6,8 +6,7 @@ namespace SharpProg.Tutorials.AzureB2C.WebClient
     {
         public Uri BaseAddress { get; set; }
         public Uri Url { get; set; }
-        public string ScopeUri { get; set; }
-        public string[] Scopes { get; set; }
+        
         public string ReadScope { get; set; }
     }
 }
